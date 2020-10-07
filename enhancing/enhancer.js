c0827d2ccdeedf6e753ce9d994ba9639d87809b0
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-06 20:40:34 -0600

    success method coded & testing complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,20 +6,45 @@ module.exports = {
 };
 
 
+// item = {
+//   "name": "item",
+//   "enhancement": "int 0-20",
+//   "durability": "int 0 - 100"
+// }
+
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
 
 function success(item) {
 
-    item.durability = 100;
-
+  if (item.enhancement <= 19) {
+    item.enhancement = (item.enhancement + 1)
+  } else {
+    console.log('enhancement already maxxed out')
+  }
 
   return { ...item };
 }
 
 function fail(item) {
+
+  if (item.enhancement < 15) {
+    item.durability = (item.durability - 5)
+  } else item.durability = (item.durability -10);
+
+  if (item.enhancement > 16) {
+    item.enhancement = (item.enhancement -1)
+  } else {
+    console.log('item enhancement is not greater than 16, not decreasing it by 1')
+  }
+
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,23 +1,65 @@
-const { success } = require('./enhancer.js');
+const { success, repair } = require('./enhancer.js');
 // test away!
 
 
-
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
 
 describe('enhancer.js', () => {
     
-    describe('success', () => {
+    describe('repair', () => {
         it('should return an item with a durability score of 100', () => {
 
             let item = {
+                name: 'item',
+                enhancement: 20,
                 durability: 50
             }
 
             const newItem = {
+                name: 'item',
+                enhancement: 20,
                 durability: 100
             };
 
-            expect(newItem).toStrictEqual(success(item));
+            expect(newItem).toStrictEqual(repair(item));
         });
+    })
+
+    describe('success', () => {
+
+        it('should increase enhancement by 1 for any enhancement values less than 20', () => {
+            let item = {
+                name: 'item',
+                enhancement: 15,
+                durability: 50
+            }
+
+            const newItem = {
+                name: 'item',
+                enhancement: 16,
+                durability: 50
+            };
+
+            expect(newItem).toStrictEqual(success(item));
+        })
+
+        it('should not add 1 to items with enhancement values of 20', () => {
+            let item = {
+                name: 'item',
+                enhancement: 20,
+                durability: 50
+            }
+
+            const newItem = {
+                name: 'item',
+                enhancement: 20,
+                durability: 50
+            };
+
+            expect(newItem).toStrictEqual(success(item));
+        })
+
     })
 })
